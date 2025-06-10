e6c986cae200900d229569315c0dfa45f48bf851
alineuwera <uwerairakozealine@gmail.com>
2025-06-10 23:11:38 +0200

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from '@/components/theme-provider';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'],display: "swap" });
 
 export const metadata: Metadata = {
   title: 'Uwera Irakoze Aline | Developer Portfolio',
